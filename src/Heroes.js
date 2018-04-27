8357417c0ea8b7c767947d398d9f1a294eb8664d
eastflag <eastflag@gmail.com>
2018-04-27 11:40:17 +0900

    3강 - 상태관리, 상태변화 추적, 2way 바인딩 방법
    onChange, setState로 2way 바인딩 구현

--- src/Heroes.js
+++ src/Heroes.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
 
+const hero = {
+  id: 1,
+  name: 'WinStorm'
+};
+
 class Heroes extends Component {
-    render() {
-        const hero = {
-          id: 1,
-          name: 'WinStorm'
-        };
-        return (
-          <div>
-            <p>{hero.id}</p>
-            <span>{hero.name}</span>
-          </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      hero: hero
+    };
+    this.onChange = this.onChange.bind(this);
+  }
+
+  
+  onChange(e) {
+    hero.name = e.target.value;
+    console.log(hero);
+    this.setState({
+      hero: hero
+    });
+  }
+  
+  render() {
+    return (
+      <div>
+        <p>{hero.id}</p>
+        <span>{hero.name}</span>
+        <div>
+          <label>name:
+            <input placeholder="name" value={this.state.hero.name} onChange={this.onChange}></input>
+          </label>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Heroes;
