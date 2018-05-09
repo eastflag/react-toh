b3218b70f952455ba305db63491fa26335c2313b
eastflag <eastflag@gmail.com>
2018-05-10 02:12:43 +0900

    7강 router - home, heroes 라우팅 메뉴 작성중

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, {Component} from 'react';
 import './App.css';
 import Heroes from "./heroes/Heroes";
+import {Route, Router, Switch} from "react-router-dom";
+import Header from "./header/Header";
+import Home from "./home/Home";
 
 class App extends Component {
   render() {
     const title = 'React App';
     return (
-      <div className="App">
-        <h1>{title}</h1>
-        <Heroes></Heroes>
-      </div>
+        <Router>
+            <Header></Header>
+            <Switch>
+                <Route exact path="/" Component={Home}></Route>
+                <Route path="/heroes" Component={Heroes}></Route>
+            </Switch>
+        </Router>
     );
   }
 }
