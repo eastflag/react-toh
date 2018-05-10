7958c4ca5cdcb60a3e06646747595994ec0bde56
eastflag <eastflag@gmail.com>
2018-05-10 18:56:01 +0900

    7강 router 기본 라우팅 설정

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import Hero from "./hero/Hero";
 import Header from "./header/Header";
 import Dashboard from "./dashboard/Dashboard";
 
+// history 이 객체를 통해 push, replace 를 통해 다른 경로로 이동하거나 앞 뒤 페이지로 전환 할 수 있습니다.
+// location 이 객체는 현재 경로에 대한 정보를 지니고 있고 URL 쿼리 (/about?foo=bar 형식) 정보도 가지고있습니다.
+// match 이 객체에는 어떤 라우트에 매칭이 되었는지에 대한 정보가 있고 params (/about/:name 형식) 정보를 가지고있습니다.
+
 class App extends Component {
   render() {
     const title = 'React App';
--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -1,6 +1,13 @@
 import React from "react";
 
 export default class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log(props.history);
+    console.log(props.location);
+    console.log(props.match);
+  }
+  
   render() {
     return (
       <div>
