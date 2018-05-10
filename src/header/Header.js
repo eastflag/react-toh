a2e2e5cfc9594e3aa2dcd6a30b2b3903dadac607
eastflag <eastflag@gmail.com>
2018-05-10 15:22:57 +0900

    7강 헤더 내비게이션, active 링크 추가. css 추가

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, {Fragment} from "react";
+import {NavLink} from "react-router-dom";
+import './Header.css';
+
+const activeStyle = {
+  color: 'green',
+  fontWeight: 'bold'
+};
 
 class Header extends React.Component {
+    
     render() {
         return (
-            <div>
-                메뉴
-            </div>
+          <Fragment>
+            <h1>Tour Of Heroes</h1>
+            <ul className="header">
+              <li><NavLink exact to="/" activeStyle={activeStyle}>Home</NavLink></li>
+              <li><NavLink exact to="/dashboard" activeStyle={activeStyle}>Dashboard</NavLink></li>
+              <li><NavLink exact to="/heroes" activeStyle={activeStyle}>Heroes</NavLink></li>
+            </ul>
+          </Fragment>
         );
     }
 }
